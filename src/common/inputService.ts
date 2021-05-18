06f2660ae24bef53824739cc8e06d7a379641747
salvoab <salvatore.abate85@gmail.com>
2021-05-18 11:50:11 +0200

    documentazione InputService

--- src/common/inputService.ts
+++ src/common/inputService.ts
@@ -17,16 +17,27 @@ export default class InputService {
     // REGOLA GENERALE:
     // Prima tutti i metodi pubblici poi i privati e in fondo i metodi getter e setter
 
+    /**
+     * Chiude il readline
+     */
     chiudiReadline():void {
       this.rl.close();
     }
 
+    /**
+     * Chiede all'utente di inserire da terminale un nickname
+     * @returns oggetto Promise che rappresenta la stringa col nickname inserito dall'utente
+     */
     async recuperaNickname():Promise<string> {
       return new Promise((resolve) => {
         this.rl.question('Inserisci il nickname del giocatore: ', (answer:string) => resolve(answer));
       });
     }
 
+    /**
+     * Chiede all'utente, tramite terminale, se vuole continuare a giocare
+     * @returns oggetto Promise che rappresenta il booleano true se l'utente sceglie di continuare, false altrimenti
+     */
     async chiediDiContinuare():Promise<boolean> {
       return new Promise((resolve) => {
         this.rl.question('Vuoi continuare? [S/N]: ', (answer:string) => {
@@ -50,6 +61,12 @@ export default class InputService {
       }
     }*/
 
+    /**
+     * Chiede all'untente, tramite terminale, di inserire un codice formato da 5 caratteri.
+     * Se il codice inserito è di 5 caretteri la Promise risolve con il codice inserito, altrimenti rigetta con la stringa "Non valido"
+     * @param repeated Passare false se si vuole scrivere la domanda iniziale, true se si vuole che la domanda sia diversa perché chiesta non per la prima volta
+     * @returns oggetto Promise che rappresenta la stringa con il codice correttamente inserito, o il reject con la stringa "Non valido"
+     */
     public async recuperaCodice(repeated:boolean):Promise<string> {
       return new Promise((resolve, reject) => {
         let domanda:string;
